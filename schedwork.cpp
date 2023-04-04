c7807b32e5d51bd0d522997fa47fbd7aa7f1e01c
Michael Hughes <hughesmr@usc.edu>
2023-04-04 19:37:05 +0000

    fixed more tests

--- schedwork.cpp
+++ schedwork.cpp
@@ -6,24 +6,30 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-// add or remove necessary headers as you please
-
 #endif
 
+
+// add or remove necessary headers as you please
 #include "schedwork.h"
+
 
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
+
+// a constant that can be used to indicate an INVALID
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
+
+
 // Add prototypes for any helper functions here
+bool backtrack(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, std::vector<std::vector<Worker_T>>& sched, std::vector<Worker_T>& workerShifts, size_t day);
 
 
 // Add your implementation of schedule() and other helper functions here
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -37,9 +43,57 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    const size_t n = avail.size();
+    const size_t k = avail[0].size();
+    if (dailyNeed > k || dailyNeed > maxShifts) return false;
 
 
+    // Initialize empty daily schedule matrix
+    sched.resize(n);
+    for (size_t i = 0; i < n; ++i) {
+        sched[i].resize(dailyNeed);
+    }
 
 
+    // Backtracking algorithm to find valid daily schedule matrix
+    std::vector<Worker_T> workerShifts(k);
+    return backtrack(avail, dailyNeed, maxShifts, sched, workerShifts, 0);
+   
 }
 
+
+bool backtrack(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, std::vector<std::vector<Worker_T>>& sched, std::vector<Worker_T>& workerShifts, size_t day) {
+    const size_t n = avail.size();
+    const size_t k = avail[0].size();
+    if (dailyNeed > k || dailyNeed > maxShifts) return false;
+
+
+    if (day == n) return true; // All days complete
+
+
+    std::vector<Worker_T> scheduled;
+    for (size_t j = 0; j < k; ++j) {
+        if (avail[day][j] && workerShifts[j] < maxShifts) {
+            scheduled.push_back(j);
+            ++workerShifts[j];
+            if (scheduled.size() == dailyNeed) break;
+        }
+    }
+    if (scheduled.size() != dailyNeed) {
+        // Backtrack by removing last scheduled worker and trying the next one
+        while (!scheduled.empty()) {
+            const Worker_T lastWorker = scheduled.back();
+            scheduled.pop_back();
+            --workerShifts[lastWorker];
+            if (backtrack(avail, dailyNeed, maxShifts, sched, workerShifts, day - 1)) {
+                // A valid schedule has been found
+                scheduled.push_back(lastWorker);
+                ++workerShifts[lastWorker];
+                break;
+            }
+        }
+        if (scheduled.empty()) return false; // No valid schedule exists
+    }
+    sched[day] = scheduled;
+    return backtrack(avail, dailyNeed, maxShifts, sched, workerShifts, day + 1);
+}
--- wordle.cpp
+++ wordle.cpp
@@ -2,19 +2,25 @@
 // For debugging
 #include <iostream>
 // For std::remove
-#include <algorithm> 
+#include <algorithm>
 #include <map>
 #include <set>
 #endif
 
+
 #include "wordle.h"
 #include "dict-eng.h"
-using namespace std;
+//using namespace std;
+
+
 
 
 // Add prototypes of helper functions here
-
-
+void recursive_helper(const std::string& in, const std::string& floating, int position, std::string& curr, std::set<std::string>& allStrings);
+bool containsAllChars(const std::string& curr, const std::string& floating);
+void findEnglishWords(const std::set<std::string>& gibberish, const std::set<std::string>& dict, std::set<std::string>& english_set);
+bool doesContainWord(const std::set<std::string>& container, const std::string& containee);
+bool doesContainLetter(const std::string& container, char& containee);
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -22,7 +28,84 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> english_set;
+    std::set<std::string> gibberish_set;
+    std::string startword(in.length(), ' '); // start with a blank word
+    recursive_helper(in, floating, 0, startword, gibberish_set);
+    // Filter gibberish to english
+    findEnglishWords(gibberish_set, dict, english_set);
+    return english_set;
 }
 
+
 // Define any helper functions here
+//
+// All permutations of strings with the given letters
+void recursive_helper(const std::string& in, const std::string& floating, int position, std::string& curr, std::set<std::string>& allStrings) {
+    if (unsigned(position) == in.length()) {
+        // Word is full check if it has floating chars
+        if (containsAllChars(curr, floating)) {
+            // Add it to possible strings
+            allStrings.insert(curr);
+        }
+        return;
+    }
+   
+    if (in[position] != '-') {// Its a fixed element, set it accordingly
+        curr[position] = in[position];
+        recursive_helper(in, floating, position += 1, curr, allStrings);
+    }
+    else {// Try every letter
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            curr[position] = letter;
+            recursive_helper(in, floating, position + 1, curr, allStrings);
+        }
+    }
+}
+
+
+// See if the word contains all the floating chars
+bool containsAllChars(const std::string& curr, const std::string& floating) {
+    // Iterate through all characters in the floating string
+    for (char ch : floating) {
+        // Check if character is present in curr
+        //if (!doesContainLetter(curr, ch)) {
+        if (curr.find(ch) == std::string::npos){
+            // If it's not present, return false
+            return false;
+        }
+    }
+    // True if all characters are present
+    return true;
+}
+
+
+// Filter down to english words only
+void findEnglishWords(const std::set<std::string>& gibberish, const std::set<std::string>& dict, std::set<std::string>& english_set) {
+    for (const std::string& word : gibberish) {
+        //if (doesContainWord(dict, word)) {
+        if (dict.count(word) > 0){
+            english_set.insert(word);
+        }
+    }
+}
+
+
+// bool doesContainWord(const std::set<std::string>& container,const std::string& containee){
+//     for (const std::string& word : container){
+//         if (word == containee){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+// bool doesContainLetter(const std::string& container, char& containee){
+//     for (const char& letter : container){
+//         if (letter == containee){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
